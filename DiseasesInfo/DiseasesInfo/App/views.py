4ab7d6786869cae072d7cf9195377eafcc58a032
SohaSk <150521718+SohaSk@users.noreply.github.com>
2023-12-07 21:57:38 +0530

    Add files via upload

---
+++ DiseasesInfo/DiseasesInfo/App/views.py
@@ -0,0 +1,98 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from django.contrib.auth.models import User, auth
+from django.views.decorators.csrf import csrf_exempt
+from .forms import signupForm
+from .models import Diseases
+
+
+#from .admin import UserAdmin
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        fm = signupForm(request.POST)
+        #username = request.POST['username']
+        #password = request.POST['password']
+        #data = User.objects.create_user(username=username, password=password)
+        #data.save()
+        if fm.is_valid():
+            fm.save()
+
+            return redirect('home')
+    else:
+        fm = signupForm()
+    return render(request,'Signup.html',{'form':fm})
+
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user=signupForm(request.POST)
+        if user is not None:
+            #login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse("Not valid user")
+    return render(request,'login.html')
+
+
+@csrf_exempt
+def search(request):
+    if request.method=="POST":
+        searched=request.POST['searched']
+        post = Diseases.objects.filter(Disease_Name__contains=searched)
+        return render(request,'search.html',{'searched':searched,'post':post})
+    else:
+        return render(request, 'search.html', {})
+
+def dashboard(request):
+    if request.method=="POST":
+        searched=request.POST['searched']
+        post = Diseases.objects.filter(Disease_Name__contains=searched)
+        return render(request,'dashboard.html',{'searched':searched,'post':post})
+    else:
+        return render(request, 'dashboard.html', {})
+
+
+
+
+def sample(request):
+    post=Diseases.objects.all()
+    return render(request, 'sample.html', {"post": post})
+
+def dash(request):
+    return render(request,'dash.html')
+
+def search1(request):
+    return render(request,'search1.html')
+
+def search2(request):
+    return render(request,'search2.html')
+
+def search3(request):
+    return render(request,'search3.html')
+
+#def search4(request):
+   # return render(request,'search4.html')
+
+def dash2(request):
+    return render(request,'dash2.html')
+
+
+def dash4(request):
+    return render(request,'dash4.html')
+
+def dash5(request):
+    return render(request,'dash5.html')
+
+def dash6(request):
+    return render(request,'dash6.html')
